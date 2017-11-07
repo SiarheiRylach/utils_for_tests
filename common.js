7b304102b03c69dab7fab94509fc4860a1842c6d
Siarhei Rylach <s.n.rylach@gmail.com>
2017-11-08 00:40:37 +0300

    Created module with methods for tests

--- common.js
+++ common.js
@@ -4,10 +4,19 @@ const fs = require('fs');
 const shell = require('shelljs');
 
 module.exports = {
+
+    /**
+     * Change color of element to green
+     * @param elem - elem for changing color
+     */
     highlightSuccess: function (elem) {
         browser.executeScript('arguments[0].style.backgroundColor = "#00ff00"' , elem);
     },
 
+    /**
+     * Change color of element to red
+     * @param elem - elem for changing color
+     */
     highlightFail: function (elem) {
         browser.executeScript('arguments[0].style.backgroundColor = "#F20800"' , elem);
     },
@@ -15,7 +24,7 @@ module.exports = {
     createScreenshot: function () {
         utils.createDir('./screenshot/')
             .then(()=>{
-                return new Promise(()=>{
+                return new Promise((resolve, reject)=>{
                     browser.takeScreenshot().then((screen)=>{
                         let date = new Date().toLocaleString("en").replace(/[/:\s,]/g, '');
                         let filename = './screenshot/' + date + '.png';
