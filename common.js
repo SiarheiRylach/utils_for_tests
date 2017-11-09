0efa226cc5726692894773033dec9321da392b6f
Siarhei Rylach <Siarhei_Rylach@epam.com>
2017-11-09 14:40:26 +0300

    resolved

--- common.js
+++ common.js
@@ -4,34 +4,30 @@ const fs = require('fs');
 const shell = require('shelljs');
 
 module.exports = {
-
-    /**
-     * Change color of element to green
-     * @param elem - elem for changing color
-     */
     highlightSuccess: function (elem) {
         browser.executeScript('arguments[0].style.backgroundColor = "#00ff00"' , elem);
     },
 
-    /**
-     * Change color of element to red
-     * @param elem - elem for changing color
-     */
     highlightFail: function (elem) {
         browser.executeScript('arguments[0].style.backgroundColor = "#F20800"' , elem);
     },
 
     createScreenshot: function () {
-        utils.createDir('./screenshot/');
-        browser.takeScreenshot().then((screen)=>{
-            let date = new Date().toLocaleString("en").replace(/[/:\s,]/g, '');
-            let filename = './screenshot/' + date + '.png';
-            fs.writeFileSync(filename, screen, 'base64', function(err) {
-                if(err) {
-                    throw (err);
-                }
+        utils.createDir('./screenshot/')
+            .then(()=>{
+                return new Promise(()=>{
+                    browser.takeScreenshot().then((screen)=>{
+                        let date = new Date().toLocaleString("en").replace(/[/:\s,]/g, '');
+                        let filename = './screenshot/' + date + '.png';
+                        fs.writeFile(filename, screen, 'base64', function(err) {
+                            if(err) {
+                                reject(err);
+                            }
+                            resolve();
+                        });
+                    });
+                });
             });
-        });
     },
 
     /**
